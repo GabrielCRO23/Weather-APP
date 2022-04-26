c98c176e18e000f4e5dd86cd64cc7cccccc1da35
Gabriel Hrastovic <gabrielcro2369@gmail.com>
2022-04-26 05:19:50 -0400

    --all

--- javascript.js
+++ javascript.js
@@ -1,60 +1,56 @@
 const searchButton = document.querySelector('#submitButton');
 const changeButton = document.querySelector('#changeUnits')
 const temperatureDisplay = document.querySelector('#temperature')
+const CITY_COUNTRY = document.querySelector('#city-and-country')
+const details = document.querySelector('#details')
+const FEELS_LIKE = document.querySelector('#feels-like')
 const checkbox = document.querySelector('#getWeatherF')
 let CITY_NAME;
 let CURRENT_TEMP;
 let UNITS;
 
-/*
-
-searchButton.addEventListener('click', function() {
-    let CITY_NAME = document.getElementById('weather-search').value;
-    event.preventDefault()
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=570f50d767691ae0612a59f3d17eac1a&${UNITS}`, {mode: 'cors'})
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(response) {
-      temperatureDisplay.textContent = Math.floor(response.main.temp)
-      
-    });
-})
-
-temperatureDisplay.addEventListener('click', function(){
-    temperatureDisplay.textContent * 9/5 + 32
-})
-
-/*
-
-changeButton.addEventListener('click', function(){
-    temperatureDisplay.textContent * 9/5 + 32
-}) 
-*/
-
+getWeatherDefault();
+        
 searchButton.addEventListener('click', function(){
-    event.preventDefault()
+    event.preventDefault();
     if (checkbox.checked === true) {
         getWeatherF();
     } else {
         getWeatherC();
     }
-})
+});
 
 async function getWeatherC() {
     UNITS = 'units=metric'
-    let CITY_NAME = document.getElementById('weather-search').value;
+    let CITY_NAME = document.getElementById('weather-search').value || 'dubrovnik';
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=570f50d767691ae0612a59f3d17eac1a&${UNITS}`, {mode: 'cors'})
     const data = await response.json();
-    temperatureDisplay.textContent = Math.floor(data.main.temp)
+    CITY_COUNTRY.textContent = data.name.toUpperCase() + ',' + ' ' + data.sys.country
+    temperatureDisplay.textContent = Math.floor(data.main.temp) + '°C'
+    details.textContent = data.weather[0].description.toUpperCase();
+    FEELS_LIKE.textContent = 'FEELS LIKE:' + ' ' + data.main.feels_like + '°C'
   }
 
 async function getWeatherF() {
     UNITS = 'units=imperial'
-    let CITY_NAME = document.getElementById('weather-search').value;
+    let CITY_NAME = document.getElementById('weather-search').value || 'dubrovnik';
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=570f50d767691ae0612a59f3d17eac1a&${UNITS}`, {mode: 'cors'})
     const data = await response.json();
-    temperatureDisplay.textContent = Math.floor(data.main.temp)
+    CITY_COUNTRY.textContent = data.name.toUpperCase() + ',' + ' ' + data.sys.country
+    temperatureDisplay.textContent = Math.floor(data.main.temp) + '°F'
+    details.textContent = data.weather[0].description.toUpperCase();
+    FEELS_LIKE.textContent = 'FEELS LIKE:' + ' ' + data.main.feels_like + '°F' 
+}
+
+async function getWeatherDefault() {
+    UNITS = 'units=metric'
+    CITY_NAME = 'dubrovnik'
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=570f50d767691ae0612a59f3d17eac1a&${UNITS}`, {mode: 'cors'})
+    const data = await response.json();
+    CITY_COUNTRY.textContent = data.name.toUpperCase() + ',' + ' ' + data.sys.country
+    temperatureDisplay.textContent = Math.floor(data.main.temp) + '°C'
+    details.textContent = data.weather[0].description.toUpperCase();
+    FEELS_LIKE.textContent = 'FEELS LIKE:' + ' ' + data.main.feels_like + '°C'
 }
 
 
